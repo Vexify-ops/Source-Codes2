72e7f10b05089af3326b732711ed56cae87852fc
Fienemann <vexisawesome1716@outlook.com>
2024-12-18 15:39:07 -0500

    kerferg's 1st effect, not really

--- plgblt.cs
+++ plgblt.cs
@@ -2,7 +2,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-class GDIRandomPolygonEffect
+class PlgBltEffect
 {
     // Import necessary GDI functions
     [DllImport("gdi32.dll", SetLastError = true)]
